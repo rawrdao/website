2ba9adbbe86fb4ad41f99ca9f4bb793df1fb0e4c
cooljinx <cooljinx2@gmail.com>
2022-05-28 22:53:40 +0530

    Tiny Dino Runner 🦖

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,41 @@
+(function ($) {
+  $.fn.bmdIframe = function (options) {
+    var self = this;
+    var settings = $.extend(
+      {
+        classBtn: '.bmd-modalButton',
+        defaultW: 640,
+        defaultH: 360,
+      },
+      options
+    );
+
+    $(settings.classBtn).on('click', function (e) {
+      var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
+
+      var dataGame = {
+        src: $(this).attr('data-bmdSrc'),
+        height: $(this).attr('data-bmdHeight') || settings.defaultH,
+        width: $(this).attr('data-bmdWidth') || settings.defaultW,
+      };
+
+      if (allowFullscreen) dataGame.allowfullscreen = '';
+
+      $(self).find('iframe').attr(dataGame);
+    });
+
+    this.on('hidden.bs.modal', function () {
+      $(this).find('iframe').html('').attr('src', '');
+    });
+
+    return this;
+  };
+})(jQuery);
+
 // A $( document ).ready() block.
 $(document).ready(function () {
+  jQuery('#myModal').bmdIframe();
+
   function getRandomGradient() {
     const SL = ', 100%, 90%';
     const bgc = `linear-gradient(to left bottom, hsl(${
